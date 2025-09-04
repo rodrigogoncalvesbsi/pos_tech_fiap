282aa5f24c5bdcc580785f472de8d13a88c5e9ba
rodrigo.araujo <rodrigo.araujo@pm.df.gov.br>
2025-09-04 14:03:47 -0300

    PC do trabalho

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import os
 
-restaurantes = ['Pizza','Sushi']
+restaurantes = [{'nome':'Praça', 'categoria':'Japonesa', 'ativo': False},
+                {'nome':'Pizza Suprema', 'categoria':'Pizza', 'ativo': True},
+                {'nome':'Cantina', 'categoria':'Italiana', 'ativo': False}]
+
 
 def exibir_nome_programa():
     print("""
@@ -37,7 +40,11 @@ def exibir_subtitulo(texto):
 def cadastrar_restaurante():
     exibir_subtitulo("Cadastro de novos restaurantes.")
     nome_restaurante = input("Digite o nome do restaurante que deseja cadastrar:")
-    restaurantes.append(nome_restaurante)
+    categoria = input(f"Digite a categoria do restaurante {nome_restaurante}:")
+
+    dados_restaurante = {'nome': nome_restaurante, 'categoria':categoria, 'ativo': False}
+
+    restaurantes.append(dados_restaurante)
     print(f"\n O restaurante {nome_restaurante} foi cadastrado com sucesso!\n\n")
     voltar_menu_principal()
 
@@ -45,10 +52,31 @@ def listar_restaurantes():
     exibir_subtitulo("Listando os restaurantes:")
     
     for restaurante in restaurantes:
-        print(f". {restaurante}")
+        nome_restaurante = restaurante['nome']
+        categoria = restaurante['categoria']
+        ativo = restaurante['ativo']
+        
+        print(f"- {nome_restaurante} | {categoria} | {ativo}")
 
     voltar_menu_principal()
+
+def alternar_estado_restaurante():
+    exibir_subtitulo("Alterando estado do restaurante")
+    nome_restaurante = input("Digite o nome do restaurante que deseja alterar o estado: ")
+    restaurante_encontrado = False
     
+    for restaurante in restaurantes:
+        if nome_restaurante == restaurante['nome']:
+            restaurante_encontrado = True
+            restaurante['ativo'] = not restaurante['ativo']
+            mensagem = f'O restaurante {nome_restaurante} foi ativado com sucesso' if restaurante['ativo'] else f'O restaurante {nome_restaurante} foi desativado com sucesso'
+            print(mensagem)
+
+    if not restaurante_encontrado:
+        print('O restaurante não foi encontrado!')
+
+    voltar_menu_principal()
+
 
 def escolher_opcao():   
     try:
@@ -59,7 +87,7 @@ def escolher_opcao():
         elif opcao_escolhida == 2:
             listar_restaurantes()
         elif opcao_escolhida == 3:
-            print("Ativar restaurante.")
+            alternar_estado_restaurante()
         elif opcao_escolhida == 4:
             finalizar_app()
         else:
--- hora_da_pratica-listas_for_e_try_except.py
+++ hora_da_pratica-listas_for_e_try_except.py
@@ -39,9 +39,9 @@ print(soma)
 #4 - Utilize um loop for para imprimir os números de 1 a 10 em ordem decrescente.
 # Inverte a ordem da lista
 
-numeros.reverse()
-for numero in numeros:
-    print(numero)
+# numeros.reverse()
+# for numero in numeros:
+#     print(numero)
 
 # Começa em 9 (tamanho - 1), vai até -1, e subtrai 1 a cada passo
 for i in range(len(numeros) - 1, -1, -1):
@@ -57,6 +57,34 @@ for i in range(tamanho):
     print(f'{i+1} x {numero_tabuada} = {(i+1) * numero_tabuada}')
 
 
+# 6 - Crie uma lista de números e utilize um loop for para calcular a soma de todos os elementos. 
+# Utilize um bloco try-except para lidar com possíveis exceções.
 
+soma = 0
+#numeros.append('dois')
+
+for numero in numeros:
+    try:
+        soma = numero + soma
+    except:
+        print("Ocorreu um erro insperado!")
+
+print(f'Resultado da soma: {soma}')
+
+
+#7 - Construa um código que calcule a média dos valores em uma lista. 
+# Utilize um bloco try-except para lidar com a divisão por zero, caso a lista esteja vazia.
+
+
+media = 0 
+
+for numero in numeros:
+    media = media + numero
+try:
+    media = media / (len(numeros) + 1)
+except:
+    print("Ocorreu um erro insperado!")
+
+print(f'Resultado da média: {media}')
 
 
